2895bdb1b77fcb98f1c0bc08ef69117a18d32abe
Swankhade7 <166693702+Swankhade7@users.noreply.github.com>
2024-04-22 08:58:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all the code in this file in newer version
+
+// alert(7 + 7) //This code wont run in node as there is a different syntax in node. This only works in browser based console
+
+//data types
+let name = "sanket";//string
+let age = 29; //number
+let isLoggedIn = true ; //boolean
+let state; // underfined
+let device = null; //standalone value
+
+//symbol=> uniqueness
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
