b6b133199b255117692610d9263427126ac373c8
Swankhade7 <166693702+Swankhade7@users.noreply.github.com>
2024-04-22 12:14:52 +0000

    format

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,15 +1,14 @@
 const accountId = 1827498;
-let accountEmail = 'sanket@gmail.com';
-var accountPassword = '12345';
-accountcity = 'Pune';
-
+let accountEmail = "sanket@gmail.com";
+var accountPassword = "12345";
+accountcity = "Pune";
 
 // accountId = 2 ; not allowed
-accountEmail = 'hc@hc.com';
+accountEmail = "hc@hc.com";
 let accountState;
 
 var accountPassword = 6789;
-accountcity = 'bengaluru';
+accountcity = "bengaluru";
 console.table([accountPassword, accountcity, accountEmail, accountState]);
 
 /*
