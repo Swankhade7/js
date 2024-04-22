29a89f0ec5df19c158e42bfcf76ae3ad3e9b79f5
Swankhade7 <166693702+Swankhade7@users.noreply.github.com>
2024-04-22 10:16:16 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,35 @@
+let score = true ; 
+
+console.log(typeof score);
+
+ score = "33abc";
+
+console.log(typeof score);
+
+let isValueNumber = Number(score);
+console.log(isValueNumber);
+
+
+//"33" => 33
+//"33abc"=> NaN
+// null=> object
+//underfined=> NaN
+// true=> 1; false=>0
+// Number(variable name), String(variable name), Boolean(variable Name)
+
+let isLoggedIn = 1;
+
+let booleanValue = Boolean(isLoggedIn);
+
+console.log(booleanValue);
+
+let isLoggedOff = 0 ;
+let booleanLogOff = Boolean(isLoggedOff);
+console.log(booleanLogOff);
+
+//1=> true; 0=> false; ""=>false; "sanket"=> true
+
+let name = 123 ; 
+let convert = String(name);
+
+console.log(typeof convert);
