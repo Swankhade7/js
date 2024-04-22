261c57490f059e79c8f602043bf5605d55f8a81d
Swankhade7 <166693702+Swankhade7@users.noreply.github.com>
2024-04-22 17:11:25 +0000

    changes

---
+++ 01_basics/06_string.js
@@ -0,0 +1,6 @@
+const name = "sanket";
+const count = 78 ; 
+console.log(`Hi my name is ${name} and i have ${count} rupees`);
+
+const name1=new String("krutika");
+console.log(`${name1[3].()} is fourth index of Krutika string`);
