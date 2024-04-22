cbcf2229064ef3f3e0e730703f8057cdb58cd661
Swankhade7 <166693702+Swankhade7@users.noreply.github.com>
2024-04-22 06:44:53 +0000

    final change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1827498;
+let accountEmail = 'sanket@gmail.com';
+var accountPassword = '12345';
+accountcity = 'Pune';
+
+
+// accountId = 2 ; not allowed
+accountEmail = 'hc@hc.com';
+let accountState;
+
+var accountPassword = 6789;
+accountcity = 'bengaluru';
+console.table([accountPassword, accountcity, accountEmail, accountState]);
+
+/*
+Prefer not to use var because of scope and block scope problems
+*/
