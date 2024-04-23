89fa7247c965176c690b70e00292e80c03b5cea0
Swankhade7 <166693702+Swankhade7@users.noreply.github.com>
2024-04-23 15:56:33 +0000

    array1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,27 @@
+const myArr = [0,1,2,3,4,5,6];
+const myArr1 = new Array("remote", "bottle", "mouse");
+// console.log(myArr,myArr1);
+// console.log(myArr[3],myArr1[1]);
+
+//methods
+// myArr.push(7);//push pop works on last of elements
+// myArr.push(8);
+// myArr.pop();
+
+//myArr.unshift(1234);//shift unshift works on first of elements
+// console.log(myArr);
+// myArr.shift();
+
+// const newArr = myArr.join();
+//slice and splice
+//slice returns the portion of given array but does not manipulate the original array
+//splice returns the portion of given array and manipulates the original array
+// console.log("A", myArr);
+// console.log(myArr.slice(1,3));
+
+// console.log("B", myArr);
+// console.log(myArr.splice(1,3));
+
+// console.log("C", myArr);
+
+
