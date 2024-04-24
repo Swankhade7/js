5d5bb09f90a05aa402cf5e4059284169a0d9a26a
Swankhade7 <166693702+Swankhade7@users.noreply.github.com>
2024-04-24 12:58:39 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,37 @@
+const user = {};
+user.name = "san";
+user.id = "123abc";
+user.age = 29;
+
+// console.log(user);
+
+const anotherUser = {
+  email: "sank@google.com",
+  fullname: {
+    username: {
+      firstname: "sanket",
+      lastname: "wankhade",
+    },
+  },
+};
+
+// console.log(anotherUser.fullname.username.firstname);
+
+// const oneUser = {user, anotherUser};
+// const oneUser = Object.assign({}, user, anotherUser);
+const oneUser = { ...user, ...anotherUser };
+// console.log(oneUser);
+
+const users = [
+  { id: 1, email: "s@gmail.com" },
+  { id: 2, email: "s@gmail.com" },
+  { id: 3, email: "s@gmail.com" },
+  { id: 4, email: "s@gmail.com" },
+];
+
+// console.log(users[2].id);
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+console.log(user.hasOwnProperty("sanket"));
