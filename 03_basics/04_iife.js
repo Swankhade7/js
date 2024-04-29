402b542fb08a27308a98339f6c448c1cb9281e2a
Swankhade7 <166693702+Swankhade7@users.noreply.github.com>
2024-04-29 10:26:30 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,8 @@
+//immidieately invoked function expressions
+
+(function back (){
+    console.log(`database connected`);
+})();
+
+((num1, num2)=>
+console.log(( num1 + num2)))(672538329, 3218761238172);
