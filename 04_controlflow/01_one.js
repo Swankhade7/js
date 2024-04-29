c8e5076fbd7ffa79b4de6ffc2349c16ac46261a0
Swankhade7 <166693702+Swankhade7@users.noreply.github.com>
2024-04-29 17:52:45 +0000

    control flow

---
+++ 04_controlflow/01_one.js
@@ -0,0 +1,46 @@
+//if
+
+// if (2 !== "2"){
+//     // console.log("executed");
+// }
+
+// <, >, >=,<=, !=,===,!==
+// const temperature = 41;
+// if(temperature>=41){
+//     console.log("too hot outside");
+// }else{
+//     console.log("slightly colder these days");
+// }
+
+// const score =400;
+
+// if (score >200){
+//     const power = "megaPunch"
+//     console.log(`Unique power ${power}`);
+// }
+// console.log(`${power}`);
+
+const balance = 1000;
+
+// if(balance>500) console.log("you're rich"), console.log("wew");
+// if (balance < 500) {
+//   console.log("less than 500, poor");
+// } else if (balance < 750) {
+//   console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else{
+//     console.log("less than 1200");
+// }
+
+// const isUserLoggedIn = true ;
+// const hasDebitCard = true;
+// const googleEmailId = false;
+// const otherEmailId = true;
+// if (isUserLoggedIn && hasDebitCard && 2===2) {
+//     console.log("kar le bhai shopping");
+// }
+
+// if (googleEmailId || otherEmailId || 7!==7) {
+//     console.log("kar le bhai login");
+// }
---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,18 @@
+const user = {}
+
+// if (user) {
+//     console.log("there is a user");
+// } else {
+//     console.log("no user");
+// }
+
+// falsy values false, 0, -0, BigInt 0n, "",NaN, null, undefined
+// truthy "0", " ", "false" , [], {} , function(){}
+
+// if (user.length === 0) {
+//     console.log("array is khali");
+// }
+
+// if (Object.keys(user).length == 0) {
+//     console.log("object bhi khali bro");
+// }
