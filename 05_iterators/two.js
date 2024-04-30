0a0f74804e918b425aafe1b8bdcbf0d634cd3243
Swankhade7 <166693702+Swankhade7@users.noreply.github.com>
2024-04-30 10:40:02 +0000

    while , do while

---
+++ 05_iterators/two.js
@@ -0,0 +1,22 @@
+//while loop
+
+let i = 0;
+while (i<=10) {
+// console.log(`Value of i is ${i}`);
+    i +=2;
+}
+
+let myArr = ["batman", "superman", "Aquaman"];
+
+let arr = 0;
+while (arr< myArr.length) {
+    // console.log(`Value of arr is ${myArr[arr]}`);
+    arr = arr+1;
+}
+
+//do while
+let score = 1 ;
+do {
+    console.log(`Value of Score is ${score}`);
+    score++;
+} while (score <=10);
